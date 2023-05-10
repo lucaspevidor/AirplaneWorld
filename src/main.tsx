833efb098d47347c2d807e93b863a32da14ab34e
Lucas Pevidor <lucas.pevidor@gmail.com>
2023-05-10 20:57:15 -0300

    Installed inversifyjs to implement interface-based-design

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import "reflect-metadata";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <App />
